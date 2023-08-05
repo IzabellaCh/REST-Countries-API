3694e5922406519c6e49537f4df5c221990e6d05
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-08-05 21:01:36 +0400

    added countries list

---
+++ src/store/countries/counteries-selector.js
@@ -0,0 +1,7 @@
+export const selectedCountriesInfo = (state) => ({
+  status: state.countries.status,
+  error: state.countries.error,
+  qty: state.countries.length,
+});
+
+export const selectALLCountries = (state) => state.countries.list;
