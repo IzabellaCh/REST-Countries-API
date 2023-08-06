ddbc2525e62ea8c2bb8e17015cf38c11f6773b08
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-08-06 12:44:18 +0400

    added county neighbors

--- src/store/details/details-reducer.js
+++ src/store/details/details-reducer.js
@@ -3,12 +3,14 @@ import {
   SET_LOADING,
   SET_ERROR,
   CLEAR_DETAILS,
+  SET_NEIGHBORS,
 } from "./details-actions";
 
 const initialState = {
   currentCountry: null,
   staus: "idle",
   error: null,
+  neighbors: [],
 };
 
 export const detailsReducer = (state = initialState, { type, payload }) => {
@@ -32,6 +34,11 @@ export const detailsReducer = (state = initialState, { type, payload }) => {
         ...state,
         status: "received",
         currentCountry: payload,
+      };
+    case SET_NEIGHBORS:
+      return {
+        ...state,
+        neighbors: payload,
       };
     default:
       return state;
